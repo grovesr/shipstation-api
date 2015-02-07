440404448142f88be5e48069020007929acb497d
Rob <grovesr1@yahoo.com>
2015-02-07 09:29:18 -0500

    minor updates required for integration with UPS app

--- shipstation-api/ssapi/ssapi.py
+++ shipstation-api/ssapi/ssapi.py
@@ -8,21 +8,31 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 # Create your models here.
-class ss(object):
+class ssapi(object):
     """
     abstract class that facilitates ShipStation API calls
     """
     API_KEY=''
     API_SECRET=''
-    API_BASE_URL='https://ssapi.ssapi.com/'
+    API_ENDPOINT='https://ssapi.shipstation.com/'
     response=requests.Response()
     query=requests.Request()
-    status_code=0
+    status_msg={}
+    status_msg[200]='OK - The request was successful (some API calls may return 201 instead).'
+    status_msg[201]='Created - The request was successful and a resource was created.'
+    status_msg[204]='No Content - The request was successful but there is no representation to return (that is, the response is empty).'
+    status_msg[400]='Bad Request - The request could not be understood or was missing required parameters.'
+    status_msg[401]='Unauthorized - Authentication failed or user does not have permissions for the requested operation.'
+    status_msg[403]='Forbidden - Access denied.'
+    status_msg[404]='Not Found - Resource was not found.'
+    status_msg[405]='Method Not Allowed - Requested method is not supported for the specified resource.'
+    status_msg[429]='Too Many Requests - Exceeded ShipStation API limits. When the limit is reached, your application should stop making requests until X-Rate-Limit-Reset seconds have elapsed.'
+    status_msg[500]='Internal Server Error - ShipStation has encountered an error.'
     
-    def __init__(self,api_key='',api_secret='',api_base_url=API_BASE_URL):
+    def __init__(self,api_key='',api_secret='',api_endpoint=API_ENDPOINT):
         self.API_KEY=api_key
         self.API_SECRET=api_secret
-        self.API_BASE_URL=api_base_url
+        self.API_ENDPOINT=api_endpoint
     
     def __unicode__(self):
         return 'ss object:'+self.query()
@@ -79,7 +89,7 @@ class ss(object):
             return headerKey.keys()
         return [key]
     
-class get(ss):
+class get(ssapi):
     """
     Class to implement ShipStation API GET requests
     """
@@ -93,22 +103,22 @@ class get(ss):
     def orders(self,**kwargs):
         """
         ShipStation GET orders API
-        kwargs holds the filter criteria
-        customerName={customerName}
-        itemKeyword={itemKeyword}
-        modifyDateStart={modifyDateStart}
-        modifyDateEnd={modifyDateEnd}
-        orderDateStart={orderDateStart}
-        orderDateEnd={orderDateEnd}
-        orderNumber={orderNumber}
-        orderStatus={orderStatus}
-        paymentDateStart={paymentDateStart}
-        paymentDateEnd={paymentDateEnd}
-        storeId={storeId}
-        page={page}
-        pageSize={pageSize}
+        Args:
+            customerName={customerName}
+            itemKeyword={itemKeyword}
+            modifyDateStart={modifyDateStart}
+            modifyDateEnd={modifyDateEnd}
+            orderDateStart={orderDateStart}
+            orderDateEnd={orderDateEnd}
+            orderNumber={orderNumber}
+            orderStatus={orderStatus}
+            paymentDateStart={paymentDateStart}
+            paymentDateEnd={paymentDateEnd}
+            storeId={storeId}
+            page={page}
+            pageSize={pageSize}
         """
-        reqUrl=self.API_BASE_URL+'orders'
+        reqUrl=self.API_ENDPOINT+'orders'
         filterCriteria='?'
         for filterName,filterValue in kwargs.iteritems():
             filterCriteria+=filterName+'='+str(filterValue)+'&'
@@ -117,22 +127,24 @@ class get(ss):
             resp=requests.get(reqUrl,auth=HTTPBasicAuth(self.API_KEY,self.API_SECRET))
         except Exception as e:
             return e.message
-        if resp.status_code != 200:
-            return 'Error: status_code '+str(resp.status_code)+" invalid filter criteria "
+        if resp.status_code != 200 and resp.status_code != 201:
+            return 'Shipstation API error: '+str(resp.status_code)+' '+resp.status_msg[resp.status_code]
         self.response=resp
         return ''
     
     def orders_orderId(self,orderId):
         """
         ShipStation orders/orderId API
+        Arg:
+            orderId
         """
-        reqUrl=self.API_BASE_URL+'orders/'+str(orderId)
+        reqUrl=self.API_ENDPOINT+'orders/'+str(orderId)
         try:
             resp=requests.get(reqUrl,auth=HTTPBasicAuth(self.API_KEY,self.API_SECRET))
         except Exception as e:
             return e.message
-        if resp.status_code != 200:
-            return 'Error: status_code '+str(resp.status_code)+" invalid order number"
+        if resp.status_code != 200 and resp.status_code != 201:
+            return 'Shipstation API error: '+str(resp.status_code)+' '+self.status_msg[resp.status_code]
         self.response=resp
         return ''
     
@@ -140,41 +152,44 @@ class get(ss):
         """
         ShipStation GET warehouses API
         """
-        reqUrl=self.API_BASE_URL+'warehouses'
+        reqUrl=self.API_ENDPOINT+'warehouses'
         try:
             resp=requests.get(reqUrl,auth=HTTPBasicAuth(self.API_KEY,self.API_SECRET))
         except Exception as e:
             return e.message
-        if resp.status_code != 200:
-            return 'Error: status_code '+str(resp.status_code)+" invalid filter criteria "
+        if resp.status_code != 200 and resp.status_code != 201:
+            return 'Shipstation API error: '+str(resp.status_code)+' '+self.status_msg[resp.status_code]
         self.response=resp
         return ''
     
     def warehouses_warehousId(self,warehouseId):
         """
         ShipStation warehouse API
+        Arg:
+            warehouseId
         """
-        reqUrl=self.API_BASE_URL+'warehouses/'+str(warehouseId) 
+        reqUrl=self.API_ENDPOINT+'warehouses/'+str(warehouseId) 
         try:
             resp=requests.get(reqUrl,auth=HTTPBasicAuth(self.API_KEY,self.API_SECRET))
         except Exception as e:
             return e.message
-        if resp.status_code != 200:
-            return 'Error: status_code '+str(resp.status_code)+" invalid filter criteria "
+        if resp.status_code != 200 and resp.status_code != 201:
+            return 'Shipstation API error: '+str(resp.status_code)+' '+self.status_msg[resp.status_code]
         self.response=resp
         return ''
     
     def customers(self,**kwargs):
         """
         ShipStation GET customers API
-        stateCode=stateCode
-        countryCode=countryCode
-        tagId=tagId
-        marketplaceId=marketplaceId
-        page=page
-        pageSize=pageSize
+        Args:
+            stateCode=stateCode
+            countryCode=countryCode
+            tagId=tagId
+            marketplaceId=marketplaceId
+            page=page
+            pageSize=pageSize
         """
-        reqUrl=self.API_BASE_URL+'customers'
+        reqUrl=self.API_ENDPOINT+'customers'
         filterCriteria='?'
         for filterName,filterValue in kwargs.iteritems():
             filterCriteria+=filterName+'='+str(filterValue)+'&'
@@ -183,22 +198,24 @@ class get(ss):
             resp=requests.get(reqUrl,auth=HTTPBasicAuth(self.API_KEY,self.API_SECRET))
         except Exception as e:
             return e.message
-        if resp.status_code != 200:
-            return 'Error: status_code '+str(resp.status_code)+" invalid filter criteria "
+        if resp.status_code != 200 and resp.status_code != 201:
+            return 'Shipstation API error: '+str(resp.status_code)+' '+self.status_msg[resp.status_code]
         self.response=resp
         return ''
 
     def customers_customerId(self,customerId):
         """
         ShipStation customers/customerId API
+        Arg:
+            customerId
         """
-        reqUrl=self.API_BASE_URL+'customers/'+str(customerId) 
+        reqUrl=self.API_ENDPOINT+'customers/'+str(customerId) 
         try:
             resp=requests.get(reqUrl,auth=HTTPBasicAuth(self.API_KEY,self.API_SECRET))
         except Exception as e:
             return e.message
-        if resp.status_code != 200:
-            return 'Error: status_code '+str(resp.status_code)+" invalid filter criteria "
+        if resp.status_code != 200 and resp.status_code != 201:
+            return 'Shipstation API error: '+str(resp.status_code)+' '+self.status_msg[resp.status_code]
         self.response=resp
         return ''
     
@@ -206,35 +223,36 @@ class get(ss):
         """
         ShipStation accounts/listtags API
         """
-        reqUrl=self.API_BASE_URL+'accounts/listtags' 
+        reqUrl=self.API_ENDPOINT+'accounts/listtags' 
         try:
             resp=requests.get(reqUrl,auth=HTTPBasicAuth(self.API_KEY,self.API_SECRET))
         except Exception as e:
             return e.message
-        if resp.status_code != 200:
-            return 'Error: status_code '+str(resp.status_code)+" invalid filter criteria "
+        if resp.status_code != 200 and resp.status_code != 201:
+            return 'Shipstation API error: '+str(resp.status_code)+' '+self.status_msg[resp.status_code]
         self.response=resp
         return ''
     
     def shipments(self,**kwargs):
         """
         ShipStation GET shipments API
-        recipientName={recipientName}
-        recipientCountryCode={recipientCountryCode}
-        orderNumber={orderNumber}
-        orderId={orderId}
-        carrierCode={carrierCode}
-        serviceCode={serviceCode}
-        trackingNumber={trackingNumber}
-        shipDateStart={shipDateStart}
-        shipDateEnd={shipDateEnd}
-        voidDateStart={voidDateStart}
-        voidDateEnd={voidDateEnd}
-        includeShipmentItems={includeShipmentItems}
-        page={page}
-        pageSize={pageSize}
+        Args:
+            recipientName={recipientName}
+            recipientCountryCode={recipientCountryCode}
+            orderNumber={orderNumber}
+            orderId={orderId}
+            carrierCode={carrierCode}
+            serviceCode={serviceCode}
+            trackingNumber={trackingNumber}
+            shipDateStart={shipDateStart}
+            shipDateEnd={shipDateEnd}
+            voidDateStart={voidDateStart}
+            voidDateEnd={voidDateEnd}
+            includeShipmentItems={includeShipmentItems}
+            page={page}
+            pageSize={pageSize}
         """
-        reqUrl=self.API_BASE_URL+'shipments'
+        reqUrl=self.API_ENDPOINT+'shipments'
         filterCriteria='?'
         for filterName,filterValue in kwargs.iteritems():
             filterCriteria+=filterName+'='+str(filterValue)+'&'
@@ -243,8 +261,8 @@ class get(ss):
             resp=requests.get(reqUrl,auth=HTTPBasicAuth(self.API_KEY,self.API_SECRET))
         except Exception as e:
             return e.message
-        if resp.status_code != 200:
-            return 'Error: status_code '+str(resp.status_code)+" invalid filter criteria "
+        if resp.status_code != 200 and resp.status_code != 201:
+            return 'Shipstation API error: '+str(resp.status_code)+' '+self.status_msg[resp.status_code]
         self.response=resp
         return ''
     
@@ -252,26 +270,30 @@ class get(ss):
         """
         ShipStation GET stores API
         """
-        reqUrl=self.API_BASE_URL+'stores'
+        reqUrl=self.API_ENDPOINT+'stores'
         try:
             resp=requests.get(reqUrl,auth=HTTPBasicAuth(self.API_KEY,self.API_SECRET))
         except Exception as e:
+            print e
             return e.message
-        if resp.status_code != 200:
-            return 'Error: status_code '+str(resp.status_code)+" invalid filter criteria "
+        if resp.status_code != 200 and resp.status_code != 201:
+            return 'Shipstation API error: '+str(resp.status_code)+' '+self.status_msg[resp.status_code]
         self.response=resp
         return ''
     
     def stores_storeId(self,storeId):
         """
         ShipStation warehouse API
+        Arg:
+            storeId
         """
-        reqUrl=self.API_BASE_URL+'stores/'+str(storeId) 
+        reqUrl=self.API_ENDPOINT+'stores/'+str(storeId) 
         try:
             resp=requests.get(reqUrl,auth=HTTPBasicAuth(self.API_KEY,self.API_SECRET))
         except Exception as e:
             return e.message
-        if resp.status_code != 200:
-            return 'Error: status_code '+str(resp.status_code)+" invalid filter criteria "
+        if resp.status_code != 200 and resp.status_code != 201:
+            return 'Shipstation API error: '+str(resp.status_code)+' '+self.status_msg[resp.status_code]
         self.response=resp
         return ''
+    
